aef1c3a741180adf60f11132b5b1cbfa790da77b
ᴋᴜɴᴀʟ <154862335+KEX001@users.noreply.github.com>
2025-01-16 02:45:52 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -44,10 +44,10 @@ export default function App() {
     const [loadingText, setLoadingText] = useState("First launch takes time...");
 
     useEffect(() => {
-      // After 40 seconds, change the message
+      // After 1 minute (60 seconds), change the message
       const timeout = setTimeout(() => {
-        setLoadingText("Your vibes are almost here!"); // Optional message change after 40 seconds
-      }, 40000); // 40 seconds
+        setLoadingText("Your vibes are almost here!"); // Optional message change after 1 minute
+      }, 60000); // 60 seconds
 
       return () => {
         clearTimeout(timeout);
@@ -56,13 +56,13 @@ export default function App() {
 
     return (
       <div className="flex flex-col items-center justify-center h-full w-full bg-neutral-800">
-        <div className="flex items-center justify-center space-x-4">
-          <p className="text-xl font-semibold text-emerald-500">{loadingText}</p>
+        <div className="flex flex-col items-center justify-center space-y-4">
           <div className="loader-4">
             <div className="box1"></div>
             <div className="box2"></div>
             <div className="box3"></div>
           </div>
+          <p className="text-xl font-semibold text-emerald-500">{loadingText}</p>
         </div>
       </div>
     );
