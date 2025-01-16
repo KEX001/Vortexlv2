d67d53b9622e6ce559848a0bdc79517116aaeb94
ᴋᴜɴᴀʟ <154862335+KEX001@users.noreply.github.com>
2025-01-16 15:43:47 +0530

    Create spotifyConfig.ts

---
+++ spotifyConfig.ts
@@ -0,0 +1,4 @@
+export const SPOTIFY_CLIENT_ID = "2d3fd5ccdd3d43dda6f17864d8eb7281";
+export const SPOTIFY_CLIENT_SECRET = "48d311d8910a4531ae81205e1f754d27";
+export const SPOTIFY_AUTH_URL = "https://accounts.spotify.com/api/token";
+export const SPOTIFY_NOW_PLAYING_URL = "https://api.spotify.com/v1/me/player/currently-playing";
